fb6ab88a5e73b266d7e19ff68d62c30dd87abfde
Tahir Ahmed Syed <tahirahmedsyed3@gmail.com>
2025-09-05 14:55:10 +0300

    connected frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -5,10 +5,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5173;
 
 app.get('/',  (req: Request, res: Response) => {
-    res.send(process.env.OPENAI_API_KEY)
+    res.send('Hello World')
+});
+
+app.get('/api/hello',  (req: Request, res: Response) => {
+    res.json({message: 'Hello World'})
 });
 
 app.listen(port, () => {
